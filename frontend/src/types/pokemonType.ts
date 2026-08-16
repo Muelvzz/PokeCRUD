627be2d5764708b16f382e798da2839085373df4
Muelvin Lopez <muelvinlopez25@gmail.com>
2026-08-16 11:31:11 +0800

    PokeCRUD v.1.4.2

--- frontend/src/types/pokemonType.ts
+++ frontend/src/types/pokemonType.ts
@@ -1,3 +1,22 @@
+import bug from '../assets/icons/type-icons/bug.svg';
+import dark from '../assets/icons/type-icons/dark.svg';
+import dragon from '../assets/icons/type-icons/dragon.svg';
+import electric from '../assets/icons/type-icons/electric.svg';
+import fairy from '../assets/icons/type-icons/fairy.svg';
+import fighting from '../assets/icons/type-icons/fighting.svg';
+import fire from '../assets/icons/type-icons/fire.svg';
+import flying from '../assets/icons/type-icons/flying.svg';
+import ghost from '../assets/icons/type-icons/ghost.svg';
+import grass from '../assets/icons/type-icons/grass.svg';
+import ground from '../assets/icons/type-icons/ground.svg';
+import ice from '../assets/icons/type-icons/ice.svg';
+import normal from '../assets/icons/type-icons/normal.svg';
+import poison from '../assets/icons/type-icons/poison.svg';
+import psychic from '../assets/icons/type-icons/psychic.svg';
+import rock from '../assets/icons/type-icons/rock.svg';
+import steel from '../assets/icons/type-icons/steel.svg';
+import water from '../assets/icons/type-icons/water.svg';
+
 interface PokemonTypeData {
   type: string;
   color: string;
@@ -5,24 +24,24 @@ interface PokemonTypeData {
 }
 
 export const typeData: PokemonTypeData[] = [
-  { type: "Bug", color: "#A3CF1799" },
-  { type: "Dark", color: "#5B617099" },
-  { type: "Dragon", color: "#0F70DE99" },
-  { type: "Electric", color: "#FFE03399" },
-  { type: "Fairy", color: "#E58DBE99" },
-  { type: "Fighting", color: "#E6324B99" },
-  { type: "Fire", color: "#FF9E3399" },
-  { type: "Flying", color: "#8FA3E899" },
-  { type: "Ghost", color: "#6463D699" },
-  { type: "Grass", color: "#43CA4599" },
-  { type: "Ground", color: "#E3884399" },
-  { type: "Ice", color: "#69D2B799" },
-  { type: "Normal", color: "#858A9499" },
-  { type: "Poison", color: "#B545DB99" },
-  { type: "Psychic", color: "#FF6D6D99" },
-  { type: "Rock", color: "#C9B47C99" },
-  { type: "Steel", color: "#4899A899" },
-  { type: "Water", color: "#4DA6ED99" },
+    { type: "Bug", color: "#A3CF17", icon: bug },
+    { type: "Dark", color: "#5B6170", icon: dark },
+    { type: "Dragon", color: "#0F70DE", icon: dragon },
+    { type: "Electric", color: "#FFE033", icon: electric },
+    { type: "Fairy", color: "#E58DBE", icon: fairy },
+    { type: "Fighting", color: "#E6324B", icon: fighting },
+    { type: "Fire", color: "#FF9E33", icon: fire },
+    { type: "Flying", color: "#8FA3E8", icon: flying },
+    { type: "Ghost", color: "#6463D6", icon: ghost },
+    { type: "Grass", color: "#43CA45", icon: grass },
+    { type: "Ground", color: "#E38843", icon: ground },
+    { type: "Ice", color: "#69D2B7", icon: ice },
+    { type: "Normal", color: "#858A94", icon: normal },
+    { type: "Poison", color: "#B545DB", icon: poison },
+    { type: "Psychic", color: "#FF6D6D", icon: psychic },
+    { type: "Rock", color: "#C9B47C", icon: rock },
+    { type: "Steel", color: "#4899A8", icon: steel },
+    { type: "Water", color: "#4DA6ED", icon: water },
 ];
 
 export interface PokemonCardProps {
